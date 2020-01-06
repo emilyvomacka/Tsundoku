6d0762d46b852e8398e19bf2577b617f370f8e94
Emily Vomacka <emilyvomacka@gmail.com>
2020-01-06 15:39:17 -0800

    firestore accepting data

--- app/src/main/java/com/example/tsundoku/MainActivity.java
+++ app/src/main/java/com/example/tsundoku/MainActivity.java
@@ -1,12 +1,36 @@
 package com.example.tsundoku;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Main";
+    private EditText enterTitle;
+    private EditText enterDesc;
+    private Button saveButton;
+
+    //Keys
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_DESC = "description";
+
+    //Connection to Firestore
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     RecyclerView recyclerView;
 
@@ -19,7 +43,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        saveButton = findViewById(R.id.save_button);
         recyclerView = findViewById(R.id.recyclerView);
+        enterTitle = findViewById(R.id.edit_text_title);
+        enterDesc = findViewById(R.id.edit_text_desc);
 
         s1 = getResources().getStringArray(R.array.programming_languages);
         s2 = getResources().getStringArray(R.array.desc);
@@ -27,6 +54,34 @@ public class MainActivity extends AppCompatActivity {
         MyAdapter myAdapter = new MyAdapter(this, s1, s2, images);
         recyclerView.setAdapter(myAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+                    String title = enterTitle.getText().toString().trim();
+                    String description = enterDesc.getText().toString().trim();
+
+                    Map<String, Object> data = new HashMap<>();
+                    data.put(KEY_TITLE, title);
+                    data.put(KEY_DESC, description);
+
+                    db.collection("Unread Books")
+                            .document("First Book")
+                            .set(data)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(MainActivity.this,  "Success!!", Toast.LENGTH_LONG).show();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG, "onFailure: " + e.toString());
+                                }
+                            });
+              }
+        });
     }
 
 }
