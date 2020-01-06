e2bcd6c859f41aedb3328ff4512cc0a19dc65d76
Emily Vomacka <emilyvomacka@gmail.com>
2020-01-05 17:01:25 -0800

    setting up cardview

--- app/src/main/java/com/example/tsundoku/MainActivity.java
+++ app/src/main/java/com/example/tsundoku/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.tsundoku;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.lang.reflect.Array;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    String s1[], s2[];
+    int images[] = {R.drawable.book};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        s1 = getResources().getStringArray(R.array.programming_languages);
+        s2 = getResources().getStringArray(R.array.desc);
+
+        MyAdapter myAdapter = new MyAdapter(this, s1, s2, images);
     }
+
 }
