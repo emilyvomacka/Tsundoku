76f4f895abd5f5f48af15bb37299339ee23bc19d
Emily Vomacka <emilyvomacka@gmail.com>
2020-01-19 13:10:13 -0800

    login activity finished

--- app/src/main/java/com/example/tsundoku/LoginActivity.java
+++ app/src/main/java/com/example/tsundoku/LoginActivity.java
@@ -3,25 +3,46 @@ package com.example.tsundoku;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import util.BookApi;
 
 public class LoginActivity extends AppCompatActivity {
     private MaterialButton loginButton;
     private MaterialButton createAcctButton;
+    private TextInputLayout passwordTextInput;
+    private TextInputEditText emailEditText, passwordEditText;
 
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection("Users");
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,8 +50,12 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.login_screen);
         super.onCreate(savedInstanceState);
 
-        final TextInputLayout passwordTextInput = findViewById(R.id.password_text_input);
-        final TextInputEditText passwordEditText = findViewById(R.id.password_edit_text);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        emailEditText = findViewById(R.id.email_edit_text);
+        passwordTextInput = findViewById(R.id.password_text_input);
+        passwordEditText = findViewById(R.id.password_edit_text);
+
         loginButton = findViewById(R.id.login_button);
         createAcctButton = findViewById(R.id.create_account_button_login);
 
@@ -49,6 +74,8 @@ public class LoginActivity extends AppCompatActivity {
                     passwordTextInput.setError(getString(R.string.login_error_password));
                 } else {
                     passwordTextInput.setError(null); // Clear the error
+                    loginEmailPasswordUser(emailEditText.getText().toString().trim(),
+                            passwordEditText.getText().toString().trim());
                     Intent intent = new Intent(getBaseContext(), MainActivity.class);
                     startActivity(intent);
                 }
@@ -65,11 +92,56 @@ public class LoginActivity extends AppCompatActivity {
                 return false;
             }
         });
-
     }
 
     private boolean isPasswordValid(@Nullable Editable text) {
         return text != null && text.length() >= 6;
+    }
+
+    private void loginEmailPasswordUser(String email, String password) {
+        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+            firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        FirebaseUser user = firebaseAuth.getCurrentUser();
+                        assert user != null;
+                        String currentUserId = user.getUid();
+
+                        collectionReference.whereEqualTo("userId", currentUserId)
+                            .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                                @Override
+                                public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots,
+                                                    @Nullable FirebaseFirestoreException e) {
+                                    if (e != null) {
+                                        return;
+                                    }
+                                    assert queryDocumentSnapshots != null;
+                                    if (!queryDocumentSnapshots.isEmpty()) {
+                                        for (QueryDocumentSnapshot snapshot : queryDocumentSnapshots) {
+                                            BookApi bookApi = BookApi.getInstance();
+                                            bookApi.setUsername(snapshot.getString("username"));
+                                            bookApi.setUserId(snapshot.getString("userId"));
+
+                                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                        }
+                                    }
+                                }
+                            });
+                    }
+                })
+
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+
+        } else {
+            Toast.makeText(LoginActivity.this, "Please enter an email and password to continue",
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
 
