95b959ee3694e40d4d9fca0856e70df260c2f174
Emily Vomacka <emilyvomacka@gmail.com>
2020-01-18 21:26:23 -0800

    detailsview complete

--- app/src/main/java/com/example/tsundoku/BookDetailsActivity.java
+++ app/src/main/java/com/example/tsundoku/BookDetailsActivity.java
@@ -3,13 +3,16 @@ package com.example.tsundoku;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
+import com.squareup.picasso.Picasso;
 
 public class BookDetailsActivity extends AppCompatActivity {
 
-    private TextView title;
+    private TextView title, author, desc;
+    private ImageView coverImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +25,22 @@ public class BookDetailsActivity extends AppCompatActivity {
 
         title = findViewById(R.id.details_book_title);
         title.setText(detailsBook.getTitle());
+
+        author = findViewById(R.id.details_book_author);
+        author.setText(detailsBook.getAuthor());
+
+        desc = findViewById(R.id.details_book_desc);
+        desc.setText(detailsBook.getDescription());
+
+        coverImage = findViewById(R.id.details_book_image);
+        String imageUrl = detailsBook.getImageUrl();
+
+        //Use Picasso to download and show image
+        Picasso.get()
+                .load(imageUrl)
+                .placeholder(R.drawable.book)
+                .fit()
+                .into(this.coverImage);
     }
+
 }
